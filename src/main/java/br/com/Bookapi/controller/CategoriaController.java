2165861afab58dbccc9e7a2e4d1e151e5c64c4a6
Wesley Rosa <wesleymrosa@gmail.com>
2024-02-23 16:39:30 -0300

    Inseridos: 1) Pacote dtos com a classe CategoriaDTO, 2) Método findAll na classe CategoriaService, 3) Método findAll na classe CategoriaController.

--- src/main/java/br/com/Bookapi/controller/CategoriaController.java
+++ src/main/java/br/com/Bookapi/controller/CategoriaController.java
@@ -1,6 +1,7 @@
 package br.com.Bookapi.controller;
 
 import br.com.Bookapi.domain.Categoria;
+import br.com.Bookapi.dtos.CategoriaDTO;
 import br.com.Bookapi.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,16 +10,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
-@RequestMapping (value = "/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaController {
 
     @Autowired
     private CategoriaService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Categoria> findById (@PathVariable Long id){
+    public ResponseEntity<Categoria> findById(@PathVariable Long id) {
         Categoria obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaDTO>> findAll() {
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+
     }
 }
---
+++ src/main/java/br/com/Bookapi/dtos/CategoriaDTO.java
@@ -0,0 +1,45 @@
+package br.com.Bookapi.dtos;
+
+import br.com.Bookapi.domain.Categoria;
+
+import java.io.Serializable;
+
+public class CategoriaDTO implements Serializable {
+    private static final Long serialVersionUID = 1L;
+    private Long id;
+    private String nome;
+    private String descricao;
+
+    public CategoriaDTO() {
+    }
+
+    public CategoriaDTO(Categoria obj) {
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.descricao = obj.getDescricao();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
